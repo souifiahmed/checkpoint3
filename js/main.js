9e54c2178a67ae8ac0183ba65f40a8b2f0fa7530
souifiahmed <44749173+souifiahmed@users.noreply.github.com>
2018-11-14 16:25:20 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,39 @@
+
+function fontFunction30() {
+  var zone=document.getElementById('select');
+  if (zone.value == '1'){
+   document.getElementById('editor1').style.fontSize='10px';
+  }
+  if (zone.value == '2'){
+   document.getElementById('editor1').style.fontSize='15px';
+  }
+  if (zone.value == '3'){
+   document.getElementById('editor1').style.fontSize='20px';
+  }
+  if (zone.value == '4'){
+   document.getElementById('editor1').style.fontSize='25px';
+  }
+  if (zone.value == '5'){
+   document.getElementById('editor1').style.fontSize='30px';
+  }
+
+}
+function fontFunction40() {
+  var zone=document.getElementById('select2');
+  if (zone.value == '11'){
+   document.getElementById('editor1').style.fontFamily='serif';
+  }
+  if (zone.value == '21'){
+   document.getElementById('editor1').style.fontFamily='Comic Sans MS';
+  }
+  if (zone.value == '31'){
+   document.getElementById('editor1').style.fontFamily='arial';
+  }
+  if (zone.value == '41'){
+   document.getElementById('editor1').style.fontFamily='Georgia';
+  }
+  if (zone.value == '51'){
+   document.getElementById('editor1').style.fontFamily='Fantasy';
+  }
+
+}
